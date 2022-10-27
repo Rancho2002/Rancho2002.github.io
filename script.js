71e62ffd8448793d77e993876d4e3c1639883f98
Arijit <subhasghosh196@outlook.com>
2022-10-27 20:38:50 +0530

    fixed bugs like screen width activity, some texts

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-console.log("script running....");
-
+// console.log("script running....");
+if(screen.width<1058){
+// if(window.innerWidth<1058){
 document.querySelector(".cross").addEventListener("click", () => {
     document.querySelector(".sidebar").classList.toggle("sidebarGo");
     setTimeout(() => {   
@@ -15,3 +16,26 @@ document.querySelector(".ham").addEventListener("click", () => {
 }, 300);
   document.querySelector(".ham").style.visibility = "hidden";
 });
+}
+
+else{
+document.querySelector(".sidebar").classList.toggle("sidebarGo");
+document.querySelector(".cross").style.visibility="visible";
+document.querySelector(".ham").style.visibility = "hidden"
+
+document.querySelector(".cross").addEventListener("click", () => {
+    document.querySelector(".sidebar").classList.toggle("sidebarGo");
+    setTimeout(() => {   
+    document.querySelector(".ham").style.visibility = "visible";
+    }, 300);
+    document.querySelector(".cross").style.visibility = "hidden";
+});
+
+document.querySelector(".ham").addEventListener("click", () => {
+  document.querySelector(".sidebar").classList.toggle("sidebarGo");
+  setTimeout(() => {   
+    document.querySelector(".cross").style.visibility = "visible";
+}, 300);
+  document.querySelector(".ham").style.visibility = "hidden";
+});
+}
